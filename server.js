eef06e5ebfa8f5bd27666b340b115fa5e757ddb0
Usenmfon <usenmfonuko@gmail.com>
2022-07-01 10:34:01 +0100

    added controllers, models, routes, and configs

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cors())
 app.use("/", routes)
 
 const connectionString = `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`
+
 db.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
